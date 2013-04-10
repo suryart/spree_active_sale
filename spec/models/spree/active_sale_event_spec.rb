2f9c2a25999bbfdfb4f0fc52d1930285f4ecb343
nando <fernando.garcia@the-cocktail.com>
2013-04-10 11:19:10 +0200

    instant parameter for #live? and #live_and_active?

--- spec/models/spree/active_sale_event_spec.rb
+++ spec/models/spree/active_sale_event_spec.rb
@@ -7,5 +7,26 @@ describe Spree::ActiveSaleEvent do
     active_sale_event.save
     active_sale_event.new_record? == true
   end
+
+  context "Eventable's instace methods" do
+    let(:active_sale_event) { create :active_sale_event }
+
+    context '#live?' do
+      it "should accept the moment when the instance should be live" do
+        active_sale_event.start_date = 5.days.from_now
+        active_sale_event.end_date = 7.days.from_now
+        active_sale_event.live?(6.days.from_now).should be true
+        active_sale_event.live?.should be false
+        Time.should_not_receive(:zone)
+      end
+    end
   
+    context '#live_and_active?' do
+      it "should pass the moment to #live? if present" do
+        now = Time.now
+        active_sale_event.should_receive(:live?).with(now)
+        active_sale_event.live_and_active? now
+      end
+    end
+  end
 end
