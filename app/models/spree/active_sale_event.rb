62fc0eb0d8f31d6660553771f2a9e4bfebf4f5a0
Surya Tripathi <raj.surya19@gmail.com>
2013-04-16 12:03:43 +0530

    removed merged migration file and merged module to sale event

--- app/models/spree/active_sale_event.rb
+++ app/models/spree/active_sale_event.rb
@@ -33,12 +33,12 @@ module Spree
     # This callback basically makes sure that parents for an event lives longer.
     # Or at least parents live for the time when event is live.
     def update_parent_active_sales
-      active_sale_events = active_sale.children_and_active_sale_events
-      parents = active_sale.self_and_ancestors.flatten
+      active_sale_events = self.active_sale.children_and_active_sale_events
+      parents = self.active_sale.self_and_ancestors.flatten
       oldest_start_date = active_sale_events.select{|event| !event.start_date.blank? }.sort_by(&:start_date).first.start_date
       latest_end_date = active_sale_events.select{|event| !event.end_date.blank? }.sort_by(&:end_date).last.end_date
-      parents.select{ |parent| parent.start_date.nil? ? true : (parent.start_date > oldest_start_date) }.each{ |sale| sale.update_attributes(:start_date => oldest_start_date) }
-      parents.select{ |parent| parent.end_date.nil? ? true : (parent.end_date < latest_end_date) }.each{ |sale| sale.update_attributes(:end_date => latest_end_date) }
+      parents.each{ |parent| parent.update_attributes(:start_date => oldest_start_date) if parent.start_date.nil? ? true : (parent.start_date > oldest_start_date) }
+      parents.each{ |parent| parent.update_attributes(:end_date => latest_end_date) if parent.end_date.nil? ? true : (parent.end_date < latest_end_date) }
     end
 
     def eventable_name
--- db/migrate/20130411091742_add_discount_to_active_sale_event.rb
+++
@@ -1,5 +0,0 @@
-class AddDiscountToActiveSaleEvent < ActiveRecord::Migration
-  def change
-    add_column :spree_active_sale_events, :discount, :integer
-  end
-end
